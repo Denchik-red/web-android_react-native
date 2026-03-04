6d1221d5d4ae2db3408a96ff08c355d296a5f014
nedviga_d_yu <DenAftogen@mail.ru>
2026-03-04 10:44:49 +0300

    react-navigate

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View, Button } from 'react-native';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+// Экран 1
+function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Главная</Text>
+      <Button title="Перейти" onPress={() => navigation.navigate('Details')} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Экран 2
+function Details() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Детали</Text>
+    </View>
+  );
+}
+
+function Style() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  )
+}
+
+// Главный компонент
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Style">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen name="Style" component={Style} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
